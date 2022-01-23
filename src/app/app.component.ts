302719496eff5e6d47a76a1b0e5acec82a6e0164
Monika Yadav <moniydv@gmail.com>
2022-01-22 20:16:41 -0500

    My first app using angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'first-app';
+  username = '';
+  isUserNameEmpty = true;
+  showPassword = true;
+  log : string[] = [];
+
+  checkUsernameLength(event:any){
+    console.log(event);
+    if(event.target.value != '') {
+      this.isUserNameEmpty = false;
+    }
+  }
+
+  resetUsername() {
+    this.username = '';
+    this.isUserNameEmpty = true;
+  }
+
+  togglePasswordContent() {
+    this.showPassword = !this.showPassword;
+    let date = new Date();
+    this.log.push('you click A on '+ date.toString());
+  }
 }
